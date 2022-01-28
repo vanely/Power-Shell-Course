a1a05d165075b1d5065f274f613ee7ec09ba215d
vanely <vanelys94@gmail.com>
2022-01-28 13:40:58 -0500

    executing powershell scripts from bash scripts

--- 7-looping/for_loops.ps1
+++ 7-looping/for_loops.ps1
@@ -1,1 +1,7 @@
-Write-Host "Demo of write host cmdlet."
+$test = Get-Process
+# used to write to console, and manipulate the output, ex: colored output
+Write-Host "Demo of write host cmdlet." -BackgroundColor White -ForegroundColor Black
+
+# writes outputs to console ex: variable, or command outputs
+# Write-Output $test
+Write-Output "Sample Output" | Get-Member
